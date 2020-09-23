fd2f3a57a671efd35a047a450b7c1ad47dd1cf33
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-09-23 16:44:15 +0200

    build(server): initialize server project and configure eslint, prettier and tsconfig
    Initialize server project and configure eslint, prettier and tsconfig

---
+++ server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmVersion: 12,
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+};
